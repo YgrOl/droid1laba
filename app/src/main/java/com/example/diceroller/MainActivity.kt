fa9a36739b2a379738b59a572dd6264d2495af17
Sasha <ugrenukalx@gmail.com>
2023-03-27 08:50:41 +0300

    some fixx

--- app/src/main/java/com/example/diceroller/MainActivity.kt
+++ app/src/main/java/com/example/diceroller/MainActivity.kt
@@ -10,31 +10,39 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        // Наша кнопка з ід
         val rollButton: Button = findViewById(R.id.button1)
+
+        // Обробник клікання кнопки
         rollButton.setOnClickListener {
             rollDice()
             rollDiceNew()
         }
     }
 
+    //Результат нажатої кнопки на екрані
     private fun rollDiceNew() {
         val dice = Dice(6)
         val diceRoll = dice.roll()
+        //Оновлення результата
         val resultTextViewNew: TextView = findViewById(R.id.textView1)
         resultTextViewNew.text = diceRoll.toString()
     }
 
 
+    //Результат нажатої кнопки на екрані
     private fun rollDice() {
         val dice = Dice(6)
         val diceRoll = dice.roll()
+        //Оновлення результата
         val resultTextView: TextView = findViewById(R.id.textView)
-        val resultTextViewNew: TextView = findViewById(R.id.textView1)
         resultTextView.text = diceRoll.toString()
-        resultTextViewNew.text = diceRoll.toString()
+
     }
 }
 
+
+//Обрання рандомного  числа від 1 до 6 для кубика
 class Dice(private val numSides: Int) {
 
     fun roll(): Int {
